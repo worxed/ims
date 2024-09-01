c76ec2b513ece359941b8c9031ac3c3f981cbb42
worxed <xinfo@cytanet.com.cy>
2024-09-02 00:19:53 +0300

    adding barebone app.js

---
+++ server/app.js
@@ -0,0 +1,12 @@
+// server/index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
